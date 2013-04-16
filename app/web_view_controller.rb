dbdfea2a22292be3d12ce083be1ee7f6844a8ea1
Andrew Nesbitt <andrewnez@gmail.com>
2013-04-16 14:18:09 +0100

    Added sharing button

--- app/web_view_controller.rb
+++ app/web_view_controller.rb
@@ -7,26 +7,38 @@ class WebViewController < UIViewController
   def viewDidLoad
     self.view = UIWebView.alloc.init
     self.view.scalesPageToFit = true
-    loadURL(@story['url']) if @story
+    loadWebview if @story
   end
 
   def loadStory(story)
     @story = story
-    loadURL(@story['url'])
-    navigationItem.title = @story['title']
+    loadWebview
   end
 
   def loadComments(story)
     @story = story
-    loadURL("https://news.ycombinator.com/item?id=#{@story['id']}")
-    navigationItem.title = @story['title']
+    loadWebview("https://news.ycombinator.com/item?id=#{@story['id']}")
   end
 
-  def loadURL(url)
+  def loadWebview(url = @story['url'])
     self.view.loadRequest NSURLRequest.requestWithURL(NSURL.URLWithString(url))
+    navigationItem.title = @story['title']
+    share_button = UIBarButtonItem.alloc.initWithBarButtonSystemItem(UIBarButtonSystemItemAction, target: self, action: 'share_button_action')
+    navigationItem.setRightBarButtonItem(share_button, true)
   end
 
-  def viewWillAppear(animated)
-    navigationItem.title = @story['title'] if @story
+  def share_button_action
+    activityViewController = UIActivityViewController.alloc.initWithActivityItems(["#{@story['title']} - #{@story['url']}"], applicationActivities:nil)
+    if Device.iphone?
+      self.presentViewController(activityViewController, animated:true, completion:nil)
+    else
+      open_share_popup(activityViewController)
+    end
+  end
+
+  def open_share_popup(activityViewController)
+    rect = [[Device.screen.width, -8], [1, 1]]
+    @popup = UIPopoverController.alloc.initWithContentViewController(activityViewController)
+    @popup.presentPopoverFromRect(rect, inView:self.view, permittedArrowDirections:UIPopoverArrowDirectionUp, animated:true)
   end
 end
